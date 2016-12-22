a137e93437dd8e481f9d02bb66afb9681c16b7d7
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-12-22 10:45:10 +0100

    Initial implementation with aurora-git-version. Kind of messy at the moment.

--- src/main/groovy/ske/aurora/maven/plugins/versionnumber/SuggestNextVersionNumberMojo.groovy
+++ src/main/groovy/ske/aurora/maven/plugins/versionnumber/SuggestNextVersionNumberMojo.groovy
@@ -8,6 +8,10 @@ import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
 import org.eclipse.jgit.errors.RepositoryNotFoundException
+import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.lib.Repository
+
+import ske.aurora.gitversion.GitVersion
 
 @Mojo(name = "suggest-version", requiresDirectInvocation = true, requiresProject = true, aggregator = true, defaultPhase = LifecyclePhase.VALIDATE)
 class SuggestNextVersionNumberMojo extends AbstractMojo {
@@ -21,19 +25,49 @@ class SuggestNextVersionNumberMojo extends AbstractMojo {
   @Parameter(defaultValue = '${project.version}', required = true, readonly = true)
   String currentVersion
 
+  @Parameter(defaultValue = 'master', required = true)
+  String stipulateNextReleaseVersionForBranchesCsv
+
   @Parameter(defaultValue = '${project}', readonly = true)
   private MavenProject project;
 
   void execute() {
-    def git = getCurrentGitRepo()
-    def tags = git.tag.list()
-        .findAll { it.name.startsWith(tagBaseName) }
-        .collect { it.name - tagBaseName - ~/[^\d]*/ }
-
-    def suggestedVersion = new ReleaseVersionEvaluator(currentVersion).suggestNextReleaseVersionFrom(tags)
-
-    project.getProperties().put(accessibleFromProperty, suggestedVersion.toString())
+    String suggestedVersion = getSuggestedVersion()
+    project.getProperties().put(accessibleFromProperty, suggestedVersion)
     getLog().info("Suggested version (${suggestedVersion}) accessible from \${${accessibleFromProperty}}")
+  }
+
+  private String getSuggestedVersion() {
+
+    Grgit git = getCurrentGitRepo()
+
+    def gitVersionOptions = new GitVersion.Options(versionPrefix: tagBaseName)
+    GitVersion.Version versionFromGit = GitVersion.determineVersion(git.repository.rootDir, gitVersionOptions)
+
+    Repository repository = git.repository.jgit.repository
+    ObjectId head = repository.resolve("HEAD");
+    String currentBranch = GitVersion.getBranchName(repository, head, true, 'BRANCH_NAME')
+    List<String> stipulateNextReleaseVersionForBranches = stipulateNextReleaseVersionForBranchesCsv
+        .split(',').
+        collect { it.trim() }
+
+    String suggestedVersion
+    if (stipulateNextReleaseVersionForBranches.contains(currentBranch)) {
+      suggestedVersion = versionFromGit.version
+    } else {
+      if (versionFromGit.source == GitVersion.VersionSource.TAG) {
+        suggestedVersion = versionFromGit.version
+      } else {
+        def tags = git.tag.list()
+            .findAll { it.name.startsWith(tagBaseName) }
+            .collect { it.name - tagBaseName - ~/[^\d]*/ }
+
+        VersionNumber stipulatedReleaseVersion = new ReleaseVersionEvaluator(currentVersion).
+            suggestNextReleaseVersionFrom(tags)
+        suggestedVersion = stipulatedReleaseVersion.toString()
+      }
+    }
+    suggestedVersion
   }
 
   static Grgit getCurrentGitRepo() {
